c2774b0b07fece8a48d2bad88f050e8fd11dc1b4
rodlobcarlos <rodlobcarlos@gmail.com>
2025-02-19 11:36:52 +0100

    new changes

--- boletin 1Entreno.sql
+++ boletin 1Entreno.sql
@@ -2,4 +2,16 @@ create database boletin1Entreno;
 use boletin1Entreno;
 
 create user usuario_test;
-select usuario_test default;
+drop user usuario_test;
+create user usuario_test identified by "contraseña" account lock;
+
+use academia;
+grant select on alumnos to usuario_test;
+set password for usuario_test = "nueva_contraseña";
+rename user usuaurio_test to usuario_test2;
+
+create view info_tabla1 as 
+select alumnos.nombre , alumnos.apellido 
+from alumnos; 
+
+select*from info_tabla1;
