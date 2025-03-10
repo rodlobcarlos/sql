b947a4fe82c483fb5f54779b8afbc237addb1cbe
rodlobcarlos <rodlobcarlos@gmail.com>
2025-03-10 18:36:02 +0100

    simulacroExamen

--- simulacroExamen.sql
+++ simulacroExamen.sql
@@ -23,4 +23,27 @@ from ALUMNO
 where DNI %2 = 0;
 
 -- Ejercicio 6
-select 
+SELECT nombre, apellidos, dni,
+CONCAT( UPPER(LEFT(nombre, 1)), -- Primera letra del nombre 
+UPPER(LEFT(apellidos, 3)),  -- Tres primeras letras del segundo apellido
+RIGHT(LEFT(dni, 8), 3) -- Tres últimas cifras del DNI (sin la letra)
+) AS identificador_seneca
+FROM alumno;
+
+-- Ejercicio 7
+select*from PROFESOR
+where Nacido_en = 1 and (Nombre like "%A%" or Apellidos like "%A%");
+
+-- Ejercicio 8
+select ID_ASIG, ID_PROF
+from IMPARTE
+where ID_PROF %2 = 0;
+
+-- Ejercicio 9
+select*from PROVINCIA order by Nombre desc limit 1;
+
+-- Ejercicio 10 // POR HACER
+
+-- Ejercicio 11
+select max(Nota1 + Nota2 + Nota3) / 3 as Record_de_clasificacion
+from MATRICULADO;
